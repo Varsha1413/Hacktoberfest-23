49b960351fdbb3ffb22b6365194a9925627698bc
Varsha1413 <91167781+Varsha1413@users.noreply.github.com>
2023-10-31 16:11:54 +0530

    Create likedlist.c

---
+++ likedlist.c
@@ -0,0 +1,55 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = nullptr;
+    }
+
+    void append(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    void display() {
+        Node* current = head;
+        while (current != nullptr) {
+            std::cout << current->data << " -> ";
+            current = current->next;
+        }
+        std::cout << "nullptr" << std::endl;
+    }
+};
+
+int main() {
+    LinkedList myList;
+    myList.append(1);
+    myList.append(2);
+    myList.append(3);
+
+    std::cout << "Linked List: ";
+    myList.display();
+
+    return 0;
+}
